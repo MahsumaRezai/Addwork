50cd3c1dca0a5f7096a0065a17c171262df37b8c
unknown <mahsuma.rezai8@gmail.com>
2023-11-17 02:07:22 -0800

    add background

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import { Fragment } from "react";
 import React from 'react';
-import Home from "./Component/Home.js";
-import About from "./Component/About.js";
-import Product from "./Component/Product.js";
-import Suggection from "./Component/Suggection.js";
-import Form from "./Component/Form.js";
-import Footer from "./Component/Footer.js";
+
 
 function App(props) {
     return (
         <Fragment>
-            <Home />
-            <About />
-            <Product />
-            <Suggection />
-            <Form />
-            <Footer/>
+           
         </Fragment>
     )
 }
