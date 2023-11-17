f221e4e3e9d919ee1fc54741671ef2f908a9c22d
unknown <mahsuma.rezai8@gmail.com>
2023-11-17 05:22:52 -0800

    add last commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import { Fragment } from "react";
-import React from 'react';
-import Form from "./Component/Form";
+import React, { useState } from 'react';
+import List from './Component/List';
+import Form from './Component/Form.js';
 
+const App = () => {
+    const [courseGoals, setCourseGoals] = useState([
+        { text: 'Do all exercises!', id: 'g1' },
+        { text: 'Finish the course!', id: 'g2' }
+    ]);
 
-function App(props) {
+    const addGoalHandler = enteredText => {
+        setCourseGoals(prevGoals => {
+            const updatedGoals = [...prevGoals];
+            updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+            return updatedGoals;
+        });
+    };
+
+    const deleteItemHandler = goalId => {
+        setCourseGoals(prevGoals => {
+            const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+            return updatedGoals;
+        });
+    };
+
+    let content = (
+        <p style={{ textAlign: 'center', color: "red" , fontSize:'20px'}}>No goals found. Maybe add one?</p>
+    );
+
+    if (courseGoals.length > 0) {
+        content = (
+            <List items={courseGoals} onDeleteItem={deleteItemHandler} />
+        );
+    }
+
     return (
-        <Fragment>
-           <Form/>
-        </Fragment>
-    )
-}
+        <div>
+            <section id="goal-form">
+                <Form onAddGoal={addGoalHandler} />
+            </section>
+            <section id="goals">
+                {content}
+
+            </section>
+        </div>
+    );
+};
 
 export default App;
--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,22 +1,28 @@
-import { Fragment } from 'react';
-import classes from './Form.module.css';
-import add from './asset/add.png'
-const Form = () => {
+import React, { useState } from 'react';
+import Button from '../Component/Button.js'
+import './Form.css';
+import image from './asset/add.png'
+const Form = (props) => {
+    const [entervalue, setentervalue] = useState('');
+    const goalInputChangeHandler = (event) => {
+        setentervalue(event.target.value)
+
+    }
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        props.onAddGoal(entervalue)
+    };
     return (
-        <Fragment>
-            <form className={classes.form}>
-                <div className={classes.inputs}>
-                    <input className={classes.input} placeholder="" type="text"></input>
-                    <button>
-                        <img src={add} alt="" className={classes.add} />
-                    </button>
-
-                </div>
-
-
-            </form>
-
-        </Fragment>
-    )
+        <form onSubmit={formSubmitHandler} className='content'>
+            <div className="form-control">
+                <label>Add Your Takse</label>
+                <input type="text" onChange={goalInputChangeHandler} />
+            </div>
+            <Button type="submit">
+                <img alt="" src={image} className="image"/>
+            </Button>
+        </form>
+    );
 }
 export default Form;
